dca6146bc47119b3bc284096e21da248c6c7ecfc
utkarsh032 <utkarshraj525@gmail.com>
2024-12-18 23:32:40 +0530

    Created AddTaskForm

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,29 @@
-import './App.css'
-import Sidebar from './Components/Sidebar'
+import './App.css';
+import AddTask from './Components/AddTask';
+import Sidebar from './Components/Sidebar';
+import Navbar from './Components/Navbar';
 
 function App() {
-
   return (
     <>
-      <Sidebar />
+      <div className="flex h-screen">
+        {/* Sidebar */}
+        <Sidebar />
+
+        {/* Main Content */}
+        <div className="flex-1 flex flex-col">
+          {/* Navbar */}
+          <Navbar />
+
+          {/* Routes for content */}
+          <div className="p-4 flex-1 overflow-auto">
+            <AddTask />
+          </div>
+
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
